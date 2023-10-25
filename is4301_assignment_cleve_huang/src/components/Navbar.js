45eab1b6666ac4fa6999f4e789e86a58c5ac2a0a
Cleve Huang <hskcleve@gmail.com>
2023-10-25 22:44:29 +0800

    feat: add Google auth, log in/out

--- is4301_assignment_cleve_huang/src/components/Navbar.js
+++ is4301_assignment_cleve_huang/src/components/Navbar.js
@@ -1,4 +1,6 @@
-import { ArrowRightOnRectangleIcon } from "@heroicons/react/20/solid";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signInWithGoogle, auth, logout } from "../firebase";
+import { ArrowLeftOnRectangleIcon } from "@heroicons/react/20/solid";
 
 const navigation = [
   { name: "Home", href: "/" },
@@ -7,6 +9,7 @@ const navigation = [
 ];
 
 export default function Navbar() {
+  const [user] = useAuthState(auth);
   const getClassName = (tabname) => {
     tabname = tabname == "home" ? "" : tabname;
     const currentURL =
@@ -21,7 +24,7 @@ export default function Navbar() {
     <header className="absolute inset-x-0 top-0 z-50 back bg-orange-500">
       <nav className="flex items-center gap-10 px-6 py-4" aria-label="Global">
         <div className="flex">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <img
               className=" h-10 w-8"
               src="./NUS_coat_of_arms.png"
@@ -40,12 +43,49 @@ export default function Navbar() {
             </a>
           ))}
         </div>
-        <div className="flex ml-auto items-center gap-2 justify-end text-white hover:underline">
-          <a href="/login" className="font-semibold leading-6">
-            Login
-          </a>
-          <ArrowRightOnRectangleIcon className="h-5 "></ArrowRightOnRectangleIcon>
-        </div>
+        {user ? (
+          <div className="ml-auto flex items-center gap-2">
+            <div className="font-light flex flex-col tracking-tight text-right leading-tight">
+              <span className="text-xs">Signed in as:</span>
+              <span className="font-bold text-sm">{user.displayName}</span>
+            </div>
+            <button
+              onClick={() => {
+                logout();
+              }}
+              className="ml-auto rounded-md bg-blue-600 px-3.5 py-2.5 
+          text-sm font-semibold text-white shadow-sm hover:bg-blue-500 
+          focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
+          focus-visible:outline-indigo-600"
+            >
+              <div className="flex items-center gap-2">
+                <ArrowLeftOnRectangleIcon className="h-5"></ArrowLeftOnRectangleIcon>
+                <span>Sign out</span>
+              </div>
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={() => {
+              signInWithGoogle();
+            }}
+            className="ml-auto rounded-md bg-blue-600 px-3.5 py-2.5 
+          text-sm font-semibold text-white shadow-sm hover:bg-blue-500 
+          focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
+          focus-visible:outline-indigo-600"
+          >
+            <div className="flex items-center">
+              <img
+                className=" h-6 w-6 bg-white rounded-3xl mr-2"
+                src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
+                alt="fucky"
+              />
+              <span>
+                Login with <span className="font-bold">Google</span>
+              </span>
+            </div>
+          </button>
+        )}
       </nav>
     </header>
   );
---
+++ is4301_assignment_cleve_huang/src/firebase.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBY_AtaQihn2z2kdCQywXEyGKxuDOmg9Y",
+  authDomain: "is4301assignment-f299d.firebaseapp.com",
+  projectId: "is4301assignment-f299d",
+  storageBucket: "is4301assignment-f299d.appspot.com",
+  messagingSenderId: "89434977453",
+  appId: "1:89434977453:web:41bb83fee3c93166678224",
+  measurementId: "G-KZK0JWV8PF",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { auth, db, signInWithGoogle, logout };
